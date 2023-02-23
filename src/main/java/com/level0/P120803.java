2bc24c03831153e6354b23bb18c789fa26122494
JunTaeINC <juntea3523@naver.com>
2023-02-23 10:28:21 +0900

    코딩문제 풀이

---
+++ src/main/java/com/level0/P120803.java
@@ -0,0 +1,7 @@
+package com.level0;
+
+public class P120803 {
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
---
+++ src/test/java/com/level0/p120803Test.java
@@ -0,0 +1,27 @@
+package com.level0;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+class P120803Test {
+
+    private P120803 p120803;
+
+    @BeforeEach
+    public void setUp() {
+        p120803 = new P120803();
+    }
+
+    @Test
+    @DisplayName("num1 과 num2 합의 값을 리턴한다")
+    void test1() {
+        assertThat(p120803.sum(2,4)).isEqualTo(6);
+        assertThat(P120803.sum(2,4)).isEqualTo(6);
+
+    }
+
+}
