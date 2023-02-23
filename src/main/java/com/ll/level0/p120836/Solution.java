9ed47195a9290922b41db674bceac9a4f9fd50ca
JunTaeINC <juntea3523@naver.com>
2023-02-23 22:49:26 +0900

    순서쌍의 개수

---
+++ src/main/java/com/ll/level0/p120836/Solution.java
@@ -0,0 +1,18 @@
+package com.ll.level0.p120836;
+
+class Main {
+    public static void main(String[] args) {
+    }
+}
+
+class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        for (int i = 1; i < n + 1 ; i++) {
+            if (n % i == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
