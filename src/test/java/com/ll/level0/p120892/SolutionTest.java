f523f7686d8536dd2774a26d38a7ab4edbb9da16
JunTaeINC <juntea3523@naver.com>
2023-03-05 14:35:00 +0900

    암호 해독

---
+++ src/test/java/com/ll/level0/p120892/SolutionTest.java
@@ -0,0 +1,19 @@
+package com.ll.level0.p120892;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SolutionTest {
+
+    @Test
+    void test1() {
+        assertThat(new Solution().solution("dfjardstddetckdaccccdegk", 4)).isEqualTo("attack");
+    }
+
+    @Test
+    void test2() {
+        assertThat(new Solution().solution("pfqallllabwaoclk", 2)).isEqualTo("fallback");
+    }
+}
