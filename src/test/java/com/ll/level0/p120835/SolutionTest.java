c1cfe34e0613ae1f02f803625c371535ef58503d
JunTaeINC <juntea3523@naver.com>
2023-03-07 22:36:15 +0900

    진료순서 정하기

---
+++ src/test/java/com/ll/level0/p120835/SolutionTest.java
@@ -0,0 +1,23 @@
+package com.ll.level0.p120835;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SolutionTest {
+
+    @Test
+    void test1() {
+        assertThat(new Solution().solution(new int[]{3, 76, 34})).isEqualTo(new int[]{3, 1, 2});
+    }
+
+    @Test
+    void test2() {
+        assertThat(new Solution().solution(new int[]{1, 2, 3, 4, 5, 6, 7})).isEqualTo(new int[]{7, 6, 5, 4, 3, 2, 1});
+    }
+
+    @Test
+    void test3() {
+        assertThat(new Solution().solution(new int[]{30, 10, 23, 6, 100})).isEqualTo(new int[]{2, 4, 3, 5, 1});
+    }
+}
