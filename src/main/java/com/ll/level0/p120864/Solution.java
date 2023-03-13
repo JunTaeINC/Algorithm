ffd017190d13559b9187dc81f8630e7c0c25f71e
JunTaeINC <juntea3523@naver.com>
2023-03-13 12:59:32 +0900

    숨어있는 숫자의 덧셈 (2)

---
+++ src/main/java/com/ll/level0/p120864/Solution.java
@@ -0,0 +1,12 @@
+package com.ll.level0.p120864;
+
+class Solution {
+    public static int solution(String my_string) {
+        int answer = 0;
+        String[] abc = my_string.split("[a-zA-Z]");
+        for (String ab : abc) {
+            if (!ab.isEmpty()) answer += Integer.parseInt(ab);
+        }
+        return answer;
+    }
+}
---
+++ src/main/java/com/ll/level0/p120896/Solution.java
@@ -0,0 +1,8 @@
+package com.ll.level0.p120896;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        return answer;
+    }
+}
