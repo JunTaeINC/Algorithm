394e265f8cf9663437fcb7d5a188d08a912ddbbd
JunTaeINC <juntea3523@naver.com>
2023-03-16 08:18:55 +0900

    폰켓몬

--- src/main/java/study/p1845/Solution.java
+++ src/main/java/study/p1845/Solution.java
@@ -1,8 +1,18 @@
 package study.p1845;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        return answer;
+        List<Integer> numList = new ArrayList<>();
+        for (int num : nums) {
+            if (!numList.contains(num)) numList.add(num);
+        }
+        if (numList.size() >= nums.length / 2) {
+            return nums.length / 2;
+        } else {
+            return numList.size();
+        }
     }
 }
--- src/test/java/study/p1845/SolutionTest.java
+++ src/test/java/study/p1845/SolutionTest.java
@@ -1,7 +1,23 @@
 package study.p1845;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class SolutionTest {
 
+    @Test
+    void test1() {
+        assertThat(new Solution().solution(new int[]{3, 1, 2, 3})).isEqualTo(2);
+    }
+
+    @Test
+    void test2() {
+        assertThat(new Solution().solution(new int[]{3, 3, 3, 2, 2, 4})).isEqualTo(3);
+    }
+
+    @Test
+    void test3() {
+        assertThat(new Solution().solution(new int[]{3, 3, 3, 2, 2, 2ㅎ})).isEqualTo(2);
+    }
 }
