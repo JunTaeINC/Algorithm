33a089da39d3bdffae9c06646db9cfa2e1a0257f
JunTaeINC <juntea3523@naver.com>
2023-03-24 16:21:17 +0900

    캐릭터의 좌표

---
+++ src/test/java/com/ll/level0/p120861/SolutionTest.java
@@ -0,0 +1,21 @@
+package com.ll.level0.p120861;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SolutionTest {
+
+    @Test
+    void test1() {
+        assertThat(new Solution().solution(new String[]{"left", "right", "up", "right", "right"},
+                new int[]{11, 11})).isEqualTo(new int[]{2, 1});
+    }
+
+    @Test
+    void test2() {
+        assertThat(new Solution().solution(new String[]{"down", "down", "down", "down", "down"},
+                new int[]{7, 9})).isEqualTo(new int[]{0, -4});
+    }
+}
