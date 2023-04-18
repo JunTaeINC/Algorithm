5aef953ecaca2d06c9805c05af00986415f7a771
JunTaeINC <juntea3523@naver.com>
2023-04-18 17:02:01 +0900

    문자열 밀기

---
+++ src/test/java/com/ll/level0/p120921/SolutionTest.java
@@ -0,0 +1,28 @@
+package com.ll.level0.p120921;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SolutionTest {
+
+    @Test
+    void test1() {
+        assertThat(new Solution().solution("hello", "ohell")).isEqualTo(1);
+    }
+
+    @Test
+    void test2() {
+        assertThat(new Solution().solution("apple", "elppa")).isEqualTo(-1);
+    }
+
+    @Test
+    void test3() {
+        assertThat(new Solution().solution("atat", "tata")).isEqualTo(1);
+    }
+
+    @Test
+    void test4() {
+        assertThat(new Solution().solution("abc", "abc")).isEqualTo(0);
+    }
+}
