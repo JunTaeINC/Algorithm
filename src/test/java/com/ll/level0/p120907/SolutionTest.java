a963f1ac23c802d09f5ea0178edc05c43cda9d81
JunTaeINC <juntea3523@naver.com>
2023-04-19 11:07:31 +0900

    OX 퀴즈

---
+++ src/test/java/com/ll/level0/p120907/SolutionTest.java
@@ -0,0 +1,21 @@
+package com.ll.level0.p120907;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SolutionTest {
+
+    @Test
+    void test1() {
+        assertThat(new Solution().solution(new String[]{"3 - 4 = -3", "5 + 6 = 11"}))
+                .isEqualTo(new String[]{"X", "O"});
+    }
+
+    @Test
+    void test2() {
+        assertThat(new Solution().solution(new String[]{"19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"}))
+                .isEqualTo(new String[]{"O", "O", "X", "O"});
+    }
+
+}
