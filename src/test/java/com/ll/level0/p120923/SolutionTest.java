58bedd530838d47986d1d856fccac15f92dff4d6
JunTaeINC <juntea3523@naver.com>
2023-04-20 13:21:39 +0900

    연속된 수의 합

---
+++ src/test/java/com/ll/level0/p120923/SolutionTest.java
@@ -0,0 +1,31 @@
+package com.ll.level0.p120923;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SolutionTest {
+
+    @Test
+    void test1() {
+        assertThat(new Solution().solution(3,12)).isEqualTo(new int[]{3,4,5});
+    }
+
+    @Test
+    void test2() {
+        assertThat(new Solution().solution(3,12)).isEqualTo(new int[]{3,4,5});
+    }
+
+    @Test
+    void test3() {
+        assertThat(new Solution().solution(3,12)).isEqualTo(new int[]{3,4,5});
+    }
+
+    @Test
+    void test4() {
+        assertThat(new Solution().solution(3,12)).isEqualTo(new int[]{3,4,5});
+    }
+
+
+
+}
