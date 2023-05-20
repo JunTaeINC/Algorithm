0829c83d238446426d0bb3d23edb13c2c88897e1
JunTaeINC <juntea3523@naver.com>
2023-05-20 14:29:46 +0900

    숫자 문자열과 영단어

---
+++ src/main/java/com/ll/level1/p81301/Soluton.java
@@ -0,0 +1,39 @@
+package com.ll.level1.p81301;
+
+import java.util.List;
+
+class Solution {
+    public int solution(String s) {
+        String answer = "";
+        String str = "";
+        List<String> numbers = List.of("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+
+        String[] arr = s.split("");
+
+        for (String a : arr) {
+            if (a.matches("\\d+")) {
+                answer += a;
+                continue;
+            }
+            str += a;
+            if (numbers.contains(str)) {
+                answer += numbers.indexOf(str);
+                str = "";
+            }
+        }
+
+        return Integer.parseInt(answer);
+    }
+
+    public int solution2(String s) {
+
+        String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < numbers.length; i++) {
+            s = s.replaceAll(numbers[i], String.valueOf(i));
+        }
+
+        return Integer.parseInt(s);
+    }
+
+}
