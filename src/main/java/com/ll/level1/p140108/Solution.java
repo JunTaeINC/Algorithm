db0e22bac85b6d3c91da9df8c13ab64f6c88859f
JunTaeINC <juntea3523@naver.com>
2023-06-29 13:07:59 +0900

    문자열 나누기

---
+++ src/main/java/com/ll/level1/p140108/Solution.java
@@ -0,0 +1,30 @@
+package com.ll.level1.p140108;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int index = 0;
+        int length = s.length();
+
+        while (index < length) {
+            char cha = s.charAt(index);
+            int sameCount = 0;
+            int notSameCount = 0;
+            while (index < length) {
+                char _cha = s.charAt(index);
+
+                if (cha == _cha) {
+                    sameCount++;
+                } else {
+                    notSameCount++;
+                }
+
+                if (sameCount == notSameCount) break;
+                index++;
+            }
+            index++;
+            answer++;
+        }
+        return answer;
+    }
+}
