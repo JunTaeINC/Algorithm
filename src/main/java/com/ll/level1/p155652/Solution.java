8695965bcfbb6258c03c342c7a404f62155c5cbd
JunTaeINC <juntea3523@naver.com>
2023-07-03 11:05:13 +0900

    둘만의 암호

---
+++ src/main/java/com/ll/level1/p155652/Solution.java
@@ -0,0 +1,18 @@
+package com.ll.level1.p155652;
+
+class Solution {
+    public static String solution(String s, String skip, int index) {
+        String answer = "";
+        for (int i = 0; i < s.length(); i++) {
+            char cha = s.charAt(i);
+            int count = 0;
+            while (count < index) {
+                cha++;
+                if (cha > 122) cha -= 26;
+                if (!skip.contains(String.valueOf(cha))) count++;
+            }
+            answer += cha;
+        }
+        return answer;
+    }
+}
