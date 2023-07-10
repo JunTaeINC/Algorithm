56150e46bd59c513b7081217b8e15d244280a03d
JunTaeINC <juntea3523@naver.com>
2023-07-10 11:51:39 +0900

    다음 큰 숫자

---
+++ src/main/java/com/ll/level2/p12911/Solution.java
@@ -0,0 +1,13 @@
+package com.ll.level2.p12911;
+
+class Solution {
+    public int solution(int n) {
+        long nCount = Integer.bitCount(n);
+        int next = n + 1;
+        while (true) {
+            if (nCount == Integer.bitCount(next)) break;
+            next++;
+        }
+        return next;
+    }
+}
