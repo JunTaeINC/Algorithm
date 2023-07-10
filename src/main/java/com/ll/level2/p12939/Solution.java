e33f2f65b6e8b075f365131744c3ed1a148fc822
JunTaeINC <juntea3523@naver.com>
2023-07-10 09:30:25 +0900

    최댓값과 최솟값

---
+++ src/main/java/com/ll/level2/p12939/Solution.java
@@ -0,0 +1,13 @@
+package com.ll.level2.p12939;
+
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        int[] arr = Arrays.stream(s.split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+        return String.format("%s %s", arr[0], arr[arr.length - 1]);
+    }
+}
