00488d4cc30078f2f3a1bd2118ce48388852792a
JunTaeINC <juntea3523@naver.com>
2023-07-17 07:43:23 +0900

    N개의 최소공배수

---
+++ src/main/java/com/ll/level2/p12953/Solution.java
@@ -0,0 +1,25 @@
+package com.ll.level2.p12953;
+
+class Solution {
+    public int solution(int[] arr) {
+        int lcm = 1;
+        for (int a : arr) {
+            lcm = leastCommonMultiple(lcm, a);
+        }
+        return lcm;
+    }
+
+    private int greatestCommonDivisor(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    private int leastCommonMultiple(int a, int b) {
+        return (a * b) / greatestCommonDivisor(a, b);
+    }
+
+}
