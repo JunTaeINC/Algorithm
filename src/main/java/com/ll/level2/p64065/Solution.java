78d4329757fe8eaf3f79fbcd3ba946fbcc79fc2d
JunTaeINC <juntea3523@naver.com>
2023-07-29 13:49:28 +0900

    튜플

--- src/main/java/com/ll/level2/p64065/Solution.java
+++ src/main/java/com/ll/level2/p64065/Solution.java
@@ -6,13 +6,19 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 class Solution {
-    public int[] solution(String s) {
-        Set<String> set = new LinkedHashSet<>();
-        String[] arr = s.substring(2, s.length() - 2).split("},\\{");
-        Arrays.sort(arr, Comparator.comparingInt(String::length));
-        for (int i = 0; i < arr.length; i++) {
-            set.addAll(Arrays.asList(arr[i].split(",")));
-        }
-        return set.stream().mapToInt(Integer::parseInt).toArray();
-    }
+	public static int[] solution(String s) {
+		String[] arr = s.substring(2, s.length() - 2).split("},\\{");
+		Arrays.sort(arr, Comparator.comparingInt(String::length));
+		Set<String> set = new LinkedHashSet<>();
+
+		for (String aa : arr) {
+			String[] a = aa.split(",");
+			for (String _a : a) {
+				if (_a.matches("[0-9]+")) {
+					set.add(_a);
+				}
+			}
+		}
+		return set.stream().mapToInt(Integer::parseInt).toArray();
+	}
 }
