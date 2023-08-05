0a82056e25ebe31c7df6d70020cf3a70458b1b65
JunTaeINC <juntea3523@naver.com>
2023-08-05 14:10:39 +0900

    타겟 넘버

---
+++ src/main/java/com/ll/level2/p43165/Solution.java
@@ -0,0 +1,20 @@
+package com.ll.level2.p43165;
+
+class Solution {
+	private int answer;
+
+	public int solution(int[] numbers, int target) {
+		dfs(numbers, target, 0, 0);
+		return answer;
+	}
+
+	private void dfs(int[] numbers, int target, int index, int sum) {
+		if (index == numbers.length) {
+			if (sum == target)
+				answer++;
+		} else {
+			dfs(numbers, target, index + 1, sum + numbers[index]);
+			dfs(numbers, target, index + 1, sum - numbers[index]);
+		}
+	}
+}
