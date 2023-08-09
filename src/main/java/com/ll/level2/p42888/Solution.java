cf99f1f43a53d3d6fe49e681eaaca9f097014384
JunTaeINC <juntea3523@naver.com>
2023-08-10 08:22:43 +0900

    오픈채팅방

---
+++ src/main/java/com/ll/level2/p42888/Solution.java
@@ -0,0 +1,43 @@
+package com.ll.level2.p42888;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+	public static String[] solution(String[] record) {
+		Map<String, String> nicknames = new HashMap<>();
+		int msgCount = 0;
+
+		for (String r : record) {
+			String[] elements = r.split(" ");
+			String action = elements[0];
+			String userId = elements[1];
+
+			if (action.equals("Enter") || action.equals("Change")) {
+				nicknames.put(userId, elements[2]);
+			}
+
+			if (!action.equals("Change")) {
+				msgCount++;
+			}
+		}
+
+		String[] answer = new String[msgCount];
+
+		int index = 0;
+		for (String r : record) {
+			String[] elements = r.split(" ");
+			String action = elements[0];
+			String userId = elements[1];
+			String nickname = nicknames.get(userId);
+
+			if (action.equals("Enter")) {
+				answer[index++] = nickname + "님이 들어왔습니다.";
+			} else if (action.equals("Leave")) {
+				answer[index++] = nickname + "님이 나갔습니다.";
+			}
+		}
+
+		return answer;
+	}
+}
