44b65c0482a715d10d20cd7b67056414d6401c5b
JunTaeINC <juntea3523@naver.com>
2023-08-15 09:22:30 +0900

    스킬트리

---
+++ src/main/java/com/programmers/level2/p49993/Solution.java
@@ -0,0 +1,17 @@
+package com.programmers.level2.p49993;
+
+class Solution {
+	public static int solution(String skill, String[] skill_trees) {
+		int answer = 0;
+
+		for (String skills : skill_trees) {
+			String str = skills.replaceAll("[^" + skill + "]", "");
+
+			if (skill.indexOf(str) == 0) {
+				answer++;
+			}
+		}
+
+		return answer;
+	}
+}
