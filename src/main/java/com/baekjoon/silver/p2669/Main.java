1af81cbf57f4552dd21fa5b94773d351b42b7b1f
JunTaeINC <juntea3523@naver.com>
2023-09-08 14:48:31 +0900

    직사각형 네개의 합집합의 면적 구하기

---
+++ src/main/java/com/baekjoon/silver/p2669/Main.java
@@ -0,0 +1,39 @@
+package com.baekjoon.silver.p2669;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int count = 0;
+		boolean[][] map = new boolean[101][101];
+
+		for (int i = 0; i < 4; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+
+			for (int x = x1; x < x2; x++) {
+				for (int y = y1; y < y2; y++) {
+					map[x][y] = true;
+				}
+			}
+		}
+
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				if (map[i][j]) count++;
+			}
+		}
+
+		System.out.println(count);
+	}
+}
