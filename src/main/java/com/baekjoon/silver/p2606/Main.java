3c0bb8b156d7e75a75a47a20771e9d82f42243cc
JunTaeINC <juntea3523@naver.com>
2023-09-16 15:19:42 +0900

    바이러스

---
+++ src/main/java/com/baekjoon/silver/p2606/Main.java
@@ -0,0 +1,46 @@
+package com.baekjoon.silver.p2606;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] map;
+    static boolean[] isVisited;
+    static int count = 0;
+    static int computers;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        computers = Integer.parseInt(br.readLine());
+        int edges = Integer.parseInt(br.readLine());
+
+        map = new int[computers + 1][computers + 1];
+        isVisited = new boolean[computers + 1];
+
+        for (int i = 0; i < edges; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            map[start][end] = map[end][start] = 1;
+        }
+
+        dfs(1);
+
+        System.out.println(count - 1);
+    }
+
+    private static void dfs(int start) {
+        isVisited[start] = true;
+        count++;
+
+        for (int i = 0; i <= computers; i++) {
+            if (!isVisited[i] && map[start][i] == 1) {
+                dfs(i);
+            }
+        }
+    }
+}
