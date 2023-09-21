72649997982488bac899c8afb8b7b27d9f96c23d
JunTaeINC <juntea3523@naver.com>
2023-09-21 12:04:04 +0900

    뒤집기

---
+++ src/main/java/com/baekjoon/silver/p1439/Main.java
@@ -0,0 +1,28 @@
+package com.baekjoon.silver.p1439;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        String[] oneArr = input.split("1");
+        String[] zeroArr = input.split("0");
+
+        int one = 0;
+        int zero = 0;
+
+        for (String oo : oneArr) {
+            if (!oo.isBlank()) one++;
+        }
+
+        for (String zz : zeroArr) {
+            if (!zz.isBlank()) zero++;
+        }
+
+        System.out.println(Math.min(one, zero));
+    }
+}
