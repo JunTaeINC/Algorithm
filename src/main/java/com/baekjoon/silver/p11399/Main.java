e45c04787f472ed074d150173decaefa45ddc4d7
JunTaeINC <juntea3523@naver.com>
2023-09-21 10:51:44 +0900

    ATM

---
+++ src/main/java/com/baekjoon/silver/p11399/Main.java
@@ -0,0 +1,30 @@
+package com.baekjoon.silver.p11399;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        int stack = 0;
+
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+
+        for (int a : arr) {
+            stack += a;
+            answer += stack;
+
+        }
+
+        System.out.println(answer);
+    }
+}
