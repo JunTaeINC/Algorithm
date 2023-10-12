8d737f23c2439a82cd11c3bd900327d5b3d859c9
JunTaeINC <juntea3523@naver.com>
2023-10-12 15:41:02 +0900

    Project Teams

---
+++ src/main/java/com/baekjoon/silver/p20044/Main.java
@@ -0,0 +1,29 @@
+package com.baekjoon.silver.p20044;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] student = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+
+        int answer = Integer.MAX_VALUE;
+        int front = 0;
+        int end = student.length - 1;
+
+        for (int i = 0; i < N; i++) {
+            answer = Math.min(answer, student[front] + student[end]);
+            front++;
+            end--;
+        }
+
+        System.out.println(answer);
+    }
+}
