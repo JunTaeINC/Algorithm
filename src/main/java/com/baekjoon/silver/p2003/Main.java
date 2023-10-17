4151371d0f0453ee29619541bed58b979173f360
JunTaeINC <juntea3523@naver.com>
2023-10-17 16:44:59 +0900

    수들의 합 2

---
+++ src/main/java/com/baekjoon/silver/p2003/Main.java
@@ -0,0 +1,36 @@
+package com.baekjoon.silver.p2003;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int target = sc.nextInt();
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int start = 0, end = 0, sum = 0, answer = 0;
+        while (true) {
+            if (sum >= target) {
+                sum -= arr[start];
+                start++;
+            } else if (end == arr.length) {
+                break;
+            } else {
+                sum += arr[end];
+                end++;
+            }
+
+            if (sum == target) {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
