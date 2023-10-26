97aa0f3f09a4346743ab2904d43794499f4ba875
JunTaeINC <juntea3523@naver.com>
2023-10-26 21:54:02 +0900

    지름길

---
+++ src/main/java/com/baekjoon/silver/p1446/Main.java
@@ -0,0 +1,39 @@
+package com.baekjoon.silver.p1446;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[M + 1];
+        int[][] shortcuts = new int[N][3];
+
+        for (int i = 0; i < N; i++) {
+            shortcuts[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        Arrays.sort(shortcuts, Comparator.comparingInt(o -> o[0]));
+
+        for (int i = 1; i <= M; i++) {
+            dp[i] = dp[i - 1] + 1;
+            for (int[] shortcut : shortcuts) {
+                if (i == shortcut[1]) {
+                    dp[i] = Math.min(dp[i], dp[shortcut[0]] + shortcut[2]);
+                }
+            }
+        }
+
+        System.out.println(dp[M]);
+    }
+}
