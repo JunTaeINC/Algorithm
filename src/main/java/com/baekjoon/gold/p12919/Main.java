e1efd3d5bc669fa960997b576ae303705faa6153
JunTaeINC <juntea3523@naver.com>
2023-11-07 22:24:09 +0900

    A와 B 2

---
+++ src/main/java/com/baekjoon/gold/p12919/Main.java
@@ -0,0 +1,35 @@
+package com.baekjoon.gold.p12919;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String start = br.readLine();
+        String target = br.readLine();
+
+        System.out.println(dfs(start, target) ? 1 : 0);
+    }
+
+
+    private static boolean dfs(String start, String target) {
+        if (start.length() == target.length()) {
+            return start.equals(target);
+        }
+
+        if (target.charAt(0) == 'B' && dfs(start, new StringBuilder(target.substring(1)).reverse().toString())) {
+            return true;
+
+        }
+
+        if (target.charAt(target.length() - 1) == 'A' && dfs(start, target.substring(0, target.length() - 1))) {
+            return true;
+        }
+
+        return false;
+    }
+}
