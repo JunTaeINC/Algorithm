fb614ac6fad5b4911b0f907fcb9c1d469629dc9f
JunTaeINC <juntea3523@naver.com>
2023-11-08 09:45:40 +0900

    소수 구하기

---
+++ src/main/java/com/baekjoon/silver/p1929/Main.java
@@ -0,0 +1,36 @@
+package com.baekjoon.silver.p1929;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) sb.append(i).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        int range = (int) Math.sqrt(number);
+
+        for (int i = 2; i <= range; i++) {
+            if (number % i == 0) return false;
+        }
+
+        return true;
+    }
+}
