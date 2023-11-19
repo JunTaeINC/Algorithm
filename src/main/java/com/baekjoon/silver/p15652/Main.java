09d172c5d31d6c0137bbecd94c1531a69c1953bc
JunTaeINC <juntea3523@naver.com>
2023-11-19 22:30:31 +0900

    N과 M (4)

---
+++ src/main/java/com/baekjoon/silver/p15652/Main.java
@@ -0,0 +1,38 @@
+package com.baekjoon.silver.p15652;
+
+import java.util.Scanner;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] temp;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        temp = new int[M];
+
+        dfs(0, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int num, int depth) {
+        if (depth == M) {
+            for (int t : temp) {
+                sb.append(t).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = num; i < N; i++) {
+            temp[depth] = i + 1;
+            dfs(i, depth + 1);
+        }
+    }
+}
