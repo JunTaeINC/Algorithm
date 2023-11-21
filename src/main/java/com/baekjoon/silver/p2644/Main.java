8d72a337bc352310faf44a7206246836940779ad
JunTaeINC <juntea3523@naver.com>
2023-11-21 14:31:37 +0900

    촌수계산 (bfs)

--- src/main/java/com/baekjoon/silver/p2644/Main.java
+++ src/main/java/com/baekjoon/silver/p2644/Main.java
@@ -3,27 +3,32 @@ package com.baekjoon.silver.p2644;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static StringTokenizer st;
     static int N;
     static int end;
-    static int answer = -1;
-    static boolean[] isVisited;
+    static int[] isVisited;
     static int[][] map;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        map = new int[N + 1][N + 1];
-        isVisited = new boolean[N + 1];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         end = Integer.parseInt(st.nextToken());
 
+        isVisited = new int[N + 1];
+        map = new int[N + 1][N + 1];
+
         int T = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
@@ -31,20 +36,29 @@ public class Main {
             map[x][y] = map[y][x] = 1;
         }
 
-        dfs(start, 0);
-        System.out.println(answer);
+        bfs(start);
+
+        System.out.println(-1);
     }
 
-    private static void dfs(int now, int count) {
-        if (now == end) {
-            answer = count;
-            return;
-        }
+    private static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        isVisited[start] = 1;
 
-        isVisited[now] = true;
-        for (int i = 1; i <= N; i++) {
-            if (!isVisited[i] && map[now][i] == 1) {
-                dfs(i, count + 1);
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+
+            if (now == end) {
+                System.out.println(isVisited[now] - 1);
+                System.exit(0);
+            }
+
+            for (int i = 1; i <= N; i++) {
+                if (isVisited[i] == 0 && map[now][i] == 1) {
+                    isVisited[i] = isVisited[now] + 1;
+                    queue.add(i);
+                }
             }
         }
     }
