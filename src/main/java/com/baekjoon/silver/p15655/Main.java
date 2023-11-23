85038e0797d91bb83b58727d4bac2c0fc908f0ad
JunTaeINC <juntea3523@naver.com>
2023-11-23 23:55:10 +0900

    N과 M (6)

---
+++ src/main/java/com/baekjoon/silver/p15655/Main.java
@@ -0,0 +1,52 @@
+package com.baekjoon.silver.p15655;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] arr;
+    static int[] temp;
+    static boolean[] isVisited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        temp = new int[M];
+        isVisited = new boolean[N];
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+
+        dfs(0, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int index, int depth) {
+        if (depth == M) {
+            for (int t : temp) {
+                sb.append(t).append(" ");
+            }
+
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = index; i < N; i++) {
+            if (!isVisited[i]) {
+                isVisited[i] = true;
+                temp[depth] = arr[i];
+                dfs(i, depth + 1);
+                isVisited[i] = false;
+            }
+        }
+    }
+}
