1f7bdb6f53a9b091d669bdbaac6b82ea8b1b585a
JunTaeINC <juntea3523@naver.com>
2023-12-06 14:10:42 +0900

    토마토

---
+++ src/main/java/com/baekjoon/gold/p7576/Main.java
@@ -0,0 +1,80 @@
+package com.baekjoon.gold.p7576;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] dy = {1, 0, -1, 0};
+    static int[] dx = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int y = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+
+        int[][] tomato = new int[x][y];
+        Queue<Point> queue = new LinkedList<>();
+
+        int count = 0;
+        for (int i = 0; i < x; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < y; j++) {
+                tomato[i][j] = Integer.parseInt(st.nextToken());
+                if (tomato[i][j] == 1) queue.add(new Point(i, j, 0));
+                if (tomato[i][j] == 0) count++;
+            }
+        }
+
+        if (count == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        int day = 0;
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = point.x + dx[i];
+                int ny = point.y + dy[i];
+                if (nx < 0 || ny < 0 || nx >= x || ny >= y) {
+                    continue;
+                }
+                if (tomato[nx][ny] == 0) {
+                    tomato[nx][ny] = 1;
+                    queue.add(new Point(nx, ny, point.day + 1));
+                    day = Math.max(day, point.day + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                if (tomato[i][j] == 0) {
+                    System.out.println(-1);
+                    return;
+                }
+            }
+        }
+
+        System.out.println(day);
+    }
+
+    static class Point {
+        int x;
+        int y;
+        int day;
+
+        public Point(int x, int y, int day) {
+            this.x = x;
+            this.y = y;
+            this.day = day;
+        }
+    }
+}
