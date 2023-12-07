3ccb1cd191b3064b587ce9a0f0fa9f6b53b915ac
JunTaeINC <juntea3523@naver.com>
2023-12-07 21:06:41 +0900

    문자열 압축

---
+++ src/main/java/platform/lecture/step_1/Problem_1.java
@@ -0,0 +1,36 @@
+package platform.lecture.step_1;
+
+class Problem_1 {
+    public String solution(String string) {
+        String answer = "" + string.charAt(0);
+        int count = 0;
+        char target = string.charAt(0);
+
+        for (int i = 0; i < string.length(); i++) {
+            if (target == string.charAt(i)) {
+                count++;
+            } else {
+                if (count == 1) {
+                    answer += string.charAt(i);
+                } else {
+                    answer += String.valueOf(count) + string.charAt(i);
+                }
+
+                target = string.charAt(i);
+                count = 1;
+            }
+        }
+
+        if (count > 1) {
+            answer += count;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Problem_1 T = new Problem_1();
+        System.out.println(T.solution("KKHSSSSSSSE")); // K2HS7E
+        System.out.println(T.solution("AAABCCCDD")); // A3BC3D2
+    }
+}
