5efe0a12850e783bdb9d13b7656a655a1f2c74a9
JunTaeINC <juntea3523@naver.com>
2023-12-11 22:24:41 +0900

    특정 문자 뒤집기

---
+++ src/main/java/platform/lecture/step_1/Problem_3.java
@@ -0,0 +1,30 @@
+package platform.lecture.step_1;
+
+public class Problem_3 {
+
+    public static void main(String[] args) {
+        System.out.println(solution("a#b!GE*T@S"));   // S#T!EG*b@a
+        System.out.println(solution("###ab*@@Sty"));  // ###yt*@@Sba
+    }
+
+    private static String solution(String string) {
+        char[] chars = string.toCharArray();
+        int start = 0, end = string.length() - 1;
+
+        while (start < end) {
+            if (!Character.isLetter(chars[start])) {
+                start++;
+            } else if (!Character.isLetter(chars[end])) {
+                end--;
+            } else {
+                char temp = chars[end];
+                chars[end] = chars[start];
+                chars[start] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        return new String(chars);
+    }
+}
