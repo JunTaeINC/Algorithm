cdaa71492c70348af65c445931254a486ff4def0
JunTaeINC <juntea3523@naver.com>
2024-01-17 21:34:05 +0900

    가장 긴 증가하는 부분 수열

---
+++ src/main/java/platform/baekjoon/silver/p11053/Main.java
@@ -0,0 +1,30 @@
+package platform.baekjoon.silver.p11053;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Arrays.fill(dp, 1);
+        int max = 1;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}
