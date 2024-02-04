cebe5107745f71de73a11bd5b546b916ea185d28
JunTaeINC <juntea3523@naver.com>
2024-02-05 00:03:27 +0900

    연산자 끼워넣기 (2)

---
+++ src/main/java/platform/baekjoon/silver/p15658/Main.java
@@ -0,0 +1,49 @@
+package platform.baekjoon.silver.p15658;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static int N;
+    static int min = Integer.MAX_VALUE;
+    static int max = Integer.MIN_VALUE;
+    static int[] numbers;
+    static int[] operator;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        operator = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        backtracking(numbers[0], 1);
+
+        System.out.println(max);
+        System.out.println(min);
+    }
+
+    private static void backtracking(int num, int index) {
+        if (index == N) {
+            min = Math.min(num, min);
+            max = Math.max(num, max);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (operator[i] == 0) continue;
+            operator[i]--;
+
+            switch (i) {
+                case 0 -> backtracking(num + numbers[index], index + 1);
+                case 1 -> backtracking(num - numbers[index], index + 1);
+                case 2 -> backtracking(num * numbers[index], index + 1);
+                case 3 -> backtracking(num / numbers[index], index + 1);
+            }
+
+            operator[i]++;
+        }
+    }
+}
